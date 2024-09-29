20310802836b18153b4ac7c012b2d7caa67376ac
Wichitchai200310 <wichitchai.bo.65@ubu.ac.th>
2024-09-29 18:36:22 +0700

    first commit

--- Webdev/Webdev/settings.py
+++ Webdev/Webdev/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
