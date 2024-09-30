e626968232cc08233f7a0000595fc8e661be19b9
Wattanakit <wattanakit.pl.65@ubu.ac.th>
2024-09-30 14:38:40 +0700

    first commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Webdev\env
+set VIRTUAL_ENV=C:\Users\beer0\OneDrive\เดสก์ท็อป\webdev\env
 
 if not defined PROMPT set PROMPT=$P$G
 
